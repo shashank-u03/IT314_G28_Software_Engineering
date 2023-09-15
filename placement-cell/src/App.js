3bc84ec3134cd13dc39cae684855701e64471c5e
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-09-15 20:30:00 +0530

    update mainpage

--- placement-cell/src/App.js
+++ placement-cell/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import MainPage from './pages/MainPage';
-// import { ThemeProvider } from '@mui/material/styles';
-// import theme from './pages/theme';
 
 function App() {
   return (
     <>
-      {/* <ThemeProvider theme={theme}> */}
         <MainPage />
-      {/* </ThemeProvider> */}
     </>
   );
 }
--- placement-cell/src/pages/MainPage.js
+++ placement-cell/src/pages/MainPage.js
@@ -1,25 +1,21 @@
 import React from 'react';
-// import theme from './theme';
 import MainContainer from './MainPageContainer';
 import Header from './Header';
 import Footer from './Footer';
 import AdminLogin from './login/AdminLogin';
 import StudentLogin from './login/StudentLogin';
 import CompanyLogin from './login/CompanyLogin';
-// import { ThemeProvider } from '@mui/material/styles';
-// import theme from './theme';
+
 
 function MainPage() {
     return (
         <>
-            {/* <ThemeProvider theme={theme}> */}
-                {/* <Header /> */}
-                {/* <MainContainer /> */}
+                <Header />
+                <MainContainer />
+                <Footer/>
                 {/* <AdminLogin/> */}
                 {/* <StudentLogin/> */}
-                <CompanyLogin/>
-                {/* <Footer/> */}
-            {/* </ThemeProvider> */}
+                {/* <CompanyLogin/> */}
         </>
     );
 }
